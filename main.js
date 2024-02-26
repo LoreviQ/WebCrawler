cef4e2baf6667635956b7da18a66d93f2fcc2407
LoreviQ <lorevi@protonmail.com>
2024-02-26 17:41:23 +0000

    Created main function that takes CLI and runs the crawler

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-pass;
+const { argv } = require("node:process");
+
+function main() {
+    if (argv.length != 3) {
+        throw new Error("Invalid CLI arguments provided");
+    }
+    rootURL = argv[2];
+    console.log(`Crawling at: ${rootURL}`);
+}
+
+main();
